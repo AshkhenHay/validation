135d8e57094873c89a84a41e70d3b65932abae98
toon-restore <ashxen7771@gmail.com>
2021-11-11 19:06:38 +0400

    Initial commit

---
+++ src/validation/ValidAnnotation.java
@@ -0,0 +1,69 @@
+package validation;
+
+import validation.annotation.*;
+
+import java.lang.reflect.Field;
+import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidAnnotation {
+    private static final Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$");
+
+
+    public static void validEmail(Object email) throws IllegalAccessException {
+        Field[] fields = email.getClass().getFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            if (field.isAnnotationPresent(Email.class)) {
+                String email1 = (String) field.get(email);
+                Matcher matcher = emailPattern.matcher(email1);
+                System.out.println("The Email address " + email1 + " is " + (matcher.matches() ? " valid" : "invalid"));
+            }
+        }
+    }
+
+    public static void validAdulthood(Object adulthood) throws IllegalAccessException {
+        Field[] fields = adulthood.getClass().getFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            if (field.isAnnotationPresent(Adulthood.class)) {
+                LocalDate birthday = (LocalDate) field.get(adulthood);
+                if (LocalDate.now().getYear() - birthday.getYear() == 18) {
+                    System.out.println("You are Adulthood and you can registred");
+                } else {
+                    System.out.println("You aren`t Adulthood and you can not registred");
+                }
+            }
+        }
+    }
+
+    public static void validLenght(Object lenght) throws IllegalAccessException {
+        Field[] fields = lenght.getClass().getFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            if (field.isAnnotationPresent(Length.class)) {
+                String name = (String) field.get(lenght);
+                Length annotationLenght = field.getAnnotation(Length.class);
+                if (name.length() < annotationLenght.min() && name.length() > annotationLenght.max())
+                    System.out.println("Your name must be containt min "
+                            + annotationLenght.min() + "symbol, and max " + annotationLenght.max() + "symbol");
+            }
+        }
+    }
+
+    public static void validMaxMin(Object maxMin) throws IllegalAccessException {
+        Field[] fields = maxMin.getClass().getFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            if (field.isAnnotationPresent(Min.class) && field.isAnnotationPresent(Max.class)) {
+                int discount = (int) field.get(maxMin);
+                Min minAnnotation = field.getAnnotation(Min.class);
+                Max maxAnnotation = field.getAnnotation(Max.class);
+                if (discount > maxAnnotation.value() && discount < minAnnotation.value())
+                    System.out.println("Your discount rate is wrong. It must be greater than"
+                            + minAnnotation.value() + "  and less than" + maxAnnotation.value());
+            }
+        }
+    }
+}
---
+++ src/validation/dto/CustomerDto.java
@@ -0,0 +1,91 @@
+package validation.dto;
+
+import validation.annotation.*;
+
+import java.time.LocalDate;
+
+public class CustomerDto {
+    @Length(min=2,max=30)
+    private String name;
+    @Email
+    private String email;
+    @Adulthood
+    private LocalDate birthDay;
+    @Min(0)
+    @Max(100)
+    private int discountRate;
+
+    public CustomerDto(String name, String email, LocalDate birthDay, int discountRate) {
+        this.name = name;
+        this.email = email;
+        this.birthDay = birthDay;
+        this.discountRate = discountRate;
+    }
+
+    public CustomerDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public int getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(int discountRate) {
+        this.discountRate = discountRate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CustomerDto that = (CustomerDto) o;
+
+        if (discountRate != that.discountRate) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        return birthDay != null ? birthDay.equals(that.birthDay) : that.birthDay == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (birthDay != null ? birthDay.hashCode() : 0);
+        result = 31 * result + discountRate;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDto{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDay=" + birthDay +
+                ", discountRate=" + discountRate +
+                '}';
+    }
+}
